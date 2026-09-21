51979c769dedbcaa74327a80cc7c565e9094425b
Ryan Young <ryan.young@hiive.com>
2026-09-21 16:06:52 -0700

    use on_macos
    structured block

--- Casks/hiive.rb
+++ Casks/hiive.rb
@@ -38,8 +38,8 @@ cask "hiive" do
   end
 
   postflight_steps do
-    if OS.mac?
-      system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "#{staged_path}/hiive"]
+    on_macos do
+      run "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "{{staged_path}}/hiive"]
     end
   end
 
